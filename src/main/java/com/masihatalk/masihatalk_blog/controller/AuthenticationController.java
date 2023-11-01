782cfb1482f2a188bca0d0bf4cb8032ba9c7f2cf
Faizan Shaikh <126940093+faizanshaikh0103@users.noreply.github.com>
2023-11-01 18:51:32 +0530

    change in controller

--- src/main/java/com/masihatalk/masihatalk_blog/controller/AuthenticationController.java
+++ src/main/java/com/masihatalk/masihatalk_blog/controller/AuthenticationController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.masihatalk.masihatalk_blog.additional.ApiResponse;
@@ -35,47 +36,44 @@ public class AuthenticationController {
 	private AuthenticationService profileService;
 	@Autowired
 	private blg_user_profile profile;
-	
+
 	@PostMapping("/signup")
 	public Boolean SignUp(
-				@RequestParam("user_name") String user_name,
-				@RequestParam("email") String email,
-				@RequestParam("password") String password
-			) {
+			@RequestParam("user_name") String user_name,
+			@RequestParam("email") String email,
+			@RequestParam("password") String password) {
 		profile.setUser_name(user_name);
 		profile.setEmail(email);
 		profile.setPassword(password);
 		return profileService.SignUp(profile);
 	}
-	
+
 	@GetMapping("/getAccounts")
 	public List<blg_user_profile> getAccounts() {
 		return profileService.getAccounts();
 	}
-	
-	
+
 	@PostMapping("/login")
 	public ResponseEntity<ApiResponse> LogIn(@RequestBody blg_user_profile user) {
 		Object userprofile = profileService.LogIn(user);
-		if(userprofile == null) {
+		if (userprofile == null) {
 			System.out.println(userprofile);
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse(401, "Unauthorize", null));
 		} else {
 			return ResponseEntity.ok(new ApiResponse(200, "Login Success", userprofile));
 		}
 	}
-	
+
 	@GetMapping("/getuser/{user_id}")
 	public blg_user_profile getUserprofile(@PathVariable String user_id) {
 		return profileService.getUserprofile(user_id);
 	}
-	
+
 	@GetMapping("/checkexist/{email}")
 	public boolean checkexistemail(@PathVariable String email) {
 		return profileService.checkexistemail(email);
 	}
-	
-	
+
 	@PutMapping("/updateuser")
 	public ResponseEntity updateUserprofile(
 			@RequestParam("uid") String uid,
@@ -83,8 +81,7 @@ public class AuthenticationController {
 			@RequestParam("user_name") String user_name,
 			@RequestParam("email") String email,
 			@RequestParam("password") String password,
-			@RequestParam("profile_image") MultipartFile profile_image
-			) {
+			@RequestParam("profile_image") MultipartFile profile_image) {
 		int id = Integer.parseInt(uid);
 		blg_user_profile userprofile = new blg_user_profile();
 		userprofile.setId(id);
@@ -94,30 +91,31 @@ public class AuthenticationController {
 		try {
 			userprofile.setProfile_image(profile_image.getBytes());
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
 		userprofile.setUser_name(user_name);
 		boolean result = profileService.updateUserprofile(userprofile);
-		if(result) {
+		if (result) {
 			return ResponseEntity.ok(true);
 		} else {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(false);
 		}
 	}
-	
-	@PostMapping("/adminlogin")
+
+	@RequestMapping("/adminlogin")
 	public ResponseEntity<ApiResponse> AdminLogin(@RequestBody Admin admin) {
 		Admin ad = profileService.AdminLogin(admin);
-		if(ad != null) {
-			return ResponseEntity.ok().body(new ApiResponse(200,"success",ad));
+		if (ad != null) {
+			return ResponseEntity.ok().body(new ApiResponse(200, "success", ad));
 		} else {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse(401, "Failed", null));
 		}
 	}
+
 	@DeleteMapping("/deleteuser/{user_id}")
 	public boolean deleteUser(@PathVariable String user_id) {
 		return profileService.deleteUser(user_id);
 	}
-	
+
 }
